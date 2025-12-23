76fa4ea1736e47f36e4e5ee21257bb79f57e0794
DIVISION X42 <divisionx42@proton.me>
2025-12-23 22:15:40 +0000

    exc

--- src/components/Collectable.jsx
+++ src/components/Collectable.jsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { useGLTF } from "@react-three/drei"; // easiest for R3F
-
+import { Suspense } from "react";
 
 export default function Collectable({ position }) {
   const ref = useRef();
